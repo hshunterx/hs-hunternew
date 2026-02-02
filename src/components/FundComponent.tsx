c04443b22e482c51a40aa5ba9e2d00c5b46be085
hshunterx <herdy.salim.hs@gmail.com>
2026-02-02 18:06:47 +0700

    Update FundComponent.tsx

--- src/components/FundComponent.tsx
+++ src/components/FundComponent.tsx
@@ -1,12 +1,14 @@
+'use client';
+
 import { FundButton } from '@coinbase/onchainkit/fund';
- 
+
 export default function FundComponent() {
-  
-  return <FundButton 
-    className='bg-slate-300 font-sans text-black hover:bg-purple-500' 
-    text={"Buy Crypto"}
-    hideText={false}
-    hideIcon={true}
-    openIn={"tab"}
-    />;
+  return (
+    <div className="flex justify-center">
+      <FundButton 
+        className='bg-slate-300 font-sans text-black hover:bg-purple-500'
+        text="Buy Crypto"
+      />
+    </div>
+  );
 }

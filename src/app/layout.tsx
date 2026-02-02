4a1df9d52ac34a138b79568407dde6ba20f54a2d
hshunterx <herdy.salim.hs@gmail.com>
2026-02-02 16:58:25 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,38 +1,10 @@
-import type { Metadata } from 'next';
-import { NEXT_PUBLIC_URL } from '../config';
+import './globals.css';
+import { OnchainProviders } from '../components/OnchainProviders';
 
-import './global.css';
-import '@coinbase/onchainkit/styles.css';
-import dynamic from 'next/dynamic';
-
-const OnchainProviders = dynamic(
-  () => import('src/components/OnchainProviders'),
-  {
-    ssr: false,
-  },
-);
-
-export const viewport = {
-  width: 'device-width',
-  initialScale: 1.0,
-};
-
-export const metadata: Metadata = {
-  title: 'Base Profile',
-  description: 'Built with OnchainKit',
-  openGraph: {
-    title: 'Base Profile',
-    description: 'Built with OnchainKit',
-    images: [`${NEXT_PUBLIC_URL}/vibes/vibes-19.png`],
-  },
-};
-
-export default function RootLayout({
-  children,
-}: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="flex items-center justify-center">
+      <body>
         <OnchainProviders>{children}</OnchainProviders>
       </body>
     </html>

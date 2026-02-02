7ad6f24a65c809995972c701c12320758878b61a
hshunterx <herdy.salim.hs@gmail.com>
2026-02-02 18:42:34 +0700

    Update FundComponent.tsx

--- src/components/FundComponent.tsx
+++ src/components/FundComponent.tsx
@@ -6,9 +6,7 @@ export default function FundComponent() {
   return (
     <div className="flex justify-center">
       {/* @ts-ignore */}
-      <FundButton 
-        className='bg-slate-300 font-sans text-black hover:bg-purple-500'
-      >
+      <FundButton className='bg-slate-300 font-sans text-black hover:bg-purple-500'>
         Buy Crypto
       </FundButton>
     </div>

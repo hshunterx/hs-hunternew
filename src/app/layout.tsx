f906ea01349e79e97e8435dabdd6a4bbf4edc335
hshunterx <herdy.salim.hs@gmail.com>
2026-02-02 17:07:29 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
-import './globals.css';
+// Ubah dari './globals.css' menjadi '../globals.css'
+import '../globals.css'; 
 import { OnchainProviders } from '../components/OnchainProviders';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {

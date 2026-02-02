475e3cf6c4c08591fba1f978421782c5fc04aa91
hshunterx <herdy.salim.hs@gmail.com>
2026-02-02 18:17:02 +0700

    Update FundComponent.tsx

--- src/components/FundComponent.tsx
+++ src/components/FundComponent.tsx
@@ -5,10 +5,11 @@ import { FundButton } from '@coinbase/onchainkit/fund';
 export default function FundComponent() {
   return (
     <div className="flex justify-center">
+      {/* @ts-ignore - Mengabaikan error tipe data bigint pada library OnchainKit */}
       <FundButton 
         className='bg-slate-300 font-sans text-black hover:bg-purple-500'
         text="Buy Crypto"
       />
     </div>
   );
-} 
+}
